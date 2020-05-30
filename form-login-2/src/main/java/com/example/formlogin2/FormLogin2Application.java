3639d8fb0076e5531e97bf103671fe77f59d69af
zhenstudy <13920922817@163.com>
2020-05-30 13:57:40 +0800

    form-login2 初次提交

---
+++ form-login-2/src/main/java/com/example/formlogin2/FormLogin2Application.java
@@ -0,0 +1,13 @@
+package com.example.formlogin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormLogin2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormLogin2Application.class, args);
+    }
+
+}
